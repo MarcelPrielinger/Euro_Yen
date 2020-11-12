7e18761c55b3fa5c63638a7808faa6eb043639ca
Marcel Prielinger <marcel.prielinger@htl-wels.at>
2020-11-12 11:07:27 +0100

    Controller.java erweitert

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.util.Currency;
+
 public class Controller {
+
+    @FXML private Button btn_convert;
+    @FXML private TextField txt_euro;
+    @FXML private TextField txt_yen;
+
+    CurrencyConverte cC = new CurrencyConverte();
+
+    @FXML public void doConvertion()
+    {
+        txt_yen.setText(String.valueOf(cC.euroToYen(Double.valueOf(txt_euro.getText()))));
+    }
 }
--- src/sample/CurrencyConverte.java
+++ src/sample/CurrencyConverte.java
@@ -1,3 +1,5 @@
+package sample;
+
 public class CurrencyConverte {
 
     public double euroToYen(double euro)
